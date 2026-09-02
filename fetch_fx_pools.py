9181803b7d21e521190dd65095bcc780bf8a2169
y4wsgm8g8r-cell <y4wsgm8g8r@privaterelay.appleid.com>
2026-09-02 13:29:49 +0100

    Update fetch_fx_pools.py

--- fetch_fx_pools.py
+++ fetch_fx_pools.py
@@ -553,7 +553,7 @@ def render_html(
     </div>
 
     <div class="whale-box">
-      <p class="whale-title">Posiciones &gt; $10K en vivo</p>
+      <p class="whale-title">Movimientos &gt; $10K en vivo</p>
       <div class="whale-list" id="whale-list"></div>
     </div>
 
